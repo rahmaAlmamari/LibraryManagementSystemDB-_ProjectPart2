da8f85d015e3b329d30d68cda5ce314837fdd09e
rahmaAlmamari <alr816125@gmail.com>
2025-06-02 10:46:40 +0400

    Finishing sp_MarkBookUnavailable

--- StoredProcedures.sql
+++ StoredProcedures.sql
@@ -4,3 +4,20 @@
 --USE DB
 USE LibraryManagementSystem;
 
+--1. sp_MarkBookUnavailable(BookID) 
+--Updates availability after issuing 
+
+CREATE PROCEDURE sp_MarkBookUnavailable
+    @BookID INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    UPDATE Book 
+    SET Availability_Status = 'FALSE'
+    WHERE BookID = @BookID;
+END;
+
+--to Execute sp_MarkBookUnavailable(BookID) 
+EXEC sp_MarkBookUnavailable @BookID = 123;
+
