cc011800146570e71bb3e3765a293085c9993f24
rahmaAlmamari <alr816125@gmail.com>
2025-06-02 09:46:14 +0400

    Finishing GetBookAverageRating

--- Functions.sql
+++ Functions.sql
@@ -3,3 +3,27 @@
 
 --USE DB
 USE LibraryManagementSystem;
+
+--1. GetBookAverageRating(BookID) 
+--Returns average rating of a book 
+
+CREATE FUNCTION GetBookAverageRating(@BookID INT)
+RETURNS FLOAT
+AS
+BEGIN
+    DECLARE @AvgRating FLOAT;
+
+    SELECT @AvgRating = AVG(CAST(R.Rating AS FLOAT))
+    FROM Review R
+    JOIN Member_reviewed_books MRB ON R.ReviewID = MRB.ReviewID
+    WHERE MRB.BookID = @BookID;
+
+    RETURN ISNULL(@AvgRating, 0);
+END;
+
+--to call GetBookAverageRating
+
+SELECT dbo.GetBookAverageRating(123) AS AverageRating;
+
+
+
