64ccfeb57c215eff6e5730de20114c3b64ffb01d
rahmaAlmamari <alr816125@gmail.com>
2025-06-02 08:49:04 +0400

    Finishing Non-clustered on Name → Search by name for Library Table

--- Indexes.sql
+++ Indexes.sql
@@ -1,2 +1,12 @@
---Index creation scripts to optimize query performance 
+﻿--Index creation scripts to optimize query performance 
 
+--use DB
+USE LibraryManagementSystem;
+
+--Apply indexes to speed up commonly-used queries: 
+
+--1. Library Table:
+--1.1. Non-clustered on Name → Search by name 
+
+CREATE NONCLUSTERED INDEX IX_Library_LibraryName
+ON Library (LibraryName);
