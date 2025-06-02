892626a9e3c510297fca0f7db8994afdad8b0d88
rahmaAlmamari <alr816125@gmail.com>
2025-06-02 10:57:53 +0400

    Finishing trg_UpdateBookAvailability

--- Triggers.sql
+++ Triggers.sql
@@ -1,5 +1,23 @@
---Triggers � Real-Time Business Logic 
+﻿--Triggers – Real-Time Business Logic 
 --Triggers for enforcing business rules
 
 --USE DB 
 USE LibraryManagementSystem;
+
+--1. trg_UpdateBookAvailability 
+--After new loan -> set book to unavailable 
+
+CREATE TRIGGER trg_UpdateBookAvailability
+ON Member_books
+AFTER INSERT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    UPDATE B
+    SET B.Availability_Status = 'FALSE'
+    FROM Book B
+    JOIN INSERTED I ON B.BookID = I.BookID;
+END;
+
+
