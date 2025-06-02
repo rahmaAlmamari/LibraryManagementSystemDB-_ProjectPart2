18454fa129195b40713b9d0f881c4e7c6259845e
rahmaAlmamari <alr816125@gmail.com>
2025-06-02 13:40:52 +0400

    Finishing Subqueries for complex logic (e.g., max price per genre)

--- AdvancedAggregations.sql
+++ AdvancedAggregations.sql
@@ -19,5 +19,18 @@ HAVING AVG(CAST(B.Price AS FLOAT)) > 100
    AND COUNT(B.BookID) > 5
 ORDER BY AvgPrice DESC;
 
+--2. Subqueries for complex logic (e.g., max price per genre) 
+SELECT 
+    B.BookID,
+    B.Title,
+    B.Genre,
+    B.Price
+FROM Book B
+WHERE B.Price = (
+    SELECT MAX(B2.Price)
+    FROM Book B2
+    WHERE B2.Genre = B.Genre
+)
+ORDER BY B.Genre, B.Price DESC;
 
 
