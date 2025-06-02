ce90814f1e3b8f3676bdd0b86abffa7d23f3496e
rahmaAlmamari <alr816125@gmail.com>
2025-06-02 13:38:50 +0400

    Finishing HAVING for filtering aggregates

--- AdvancedAggregations.sql
+++ AdvancedAggregations.sql
@@ -4,3 +4,20 @@
 --USE DB
 USE LibraryManagementSystem;
 
+--Includes: 
+
+--1. HAVING for filtering aggregates 
+SELECT 
+    B.Genre,
+    AVG(CAST(B.Price AS FLOAT)) AS AvgPrice,
+    COUNT(B.BookID) AS BookCount,
+    MAX(B.Price) AS MaxPrice,
+    MIN(B.Price) AS MinPrice
+FROM Book B
+GROUP BY B.Genre
+HAVING AVG(CAST(B.Price AS FLOAT)) > 100
+   AND COUNT(B.BookID) > 5
+ORDER BY AvgPrice DESC;
+
+
+
