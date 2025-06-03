07c180f1692f347f082db1136cbc5cc7f5d3402b
rahmaAlmamari <alr816125@gmail.com>
2025-06-03 08:55:44 +0400

    Start working on  Transactions – Ensuring Consistency

---
+++ Transactions.sql
@@ -0,0 +1,8 @@
+--Transactions – Ensuring Consistency 
+--Transaction scripts for atomic operations 
+
+--USE DB
+USE LibraryManagementSystem;
+
+--Real-world transactional flows: 
+--1. Borrowing a book (loan insert + update availability) 
